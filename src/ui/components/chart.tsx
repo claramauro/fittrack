463cf4e66672d85ec0f977cd86c203ef16279ce8
Clara Mauro <maurog.clara@gmail.com>
2025-07-11 18:43:33 +0200

    ajout graph en cours

---
+++ src/ui/components/chart.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import { Card, CardContent } from "../shadcn/components/ui/card";
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "../shadcn/components/ui/chart";
+
+function generateTicks(min: number, max: number, step: number): number[] {
+    const ticks = [];
+    for (let val = min; val <= max; val += step) {
+        ticks.push(val);
+    }
+    return ticks;
+}
+
+export default function Chart() {
+    const weightData = [
+        { date: "2025-06-01", weight: 69 },
+        { date: "2025-06-05", weight: 68.5 },
+        { date: "2025-06-12", weight: 68 },
+        { date: "2025-06-20", weight: 65 },
+        { date: "2025-06-28", weight: 60 },
+    ];
+
+    // Arrondi pour le domaine Y (on arrondi sur 5)
+    const weights = weightData.map((data) => Math.round(data.weight));
+    const minRange = Math.floor(Math.min(...weights) / 5) * 5; // arrondi vers le bas
+    const maxRange = Math.ceil(Math.max(...weights) / 5) * 5; // arrondi vers le haut
+
+    // Génération des ticks tous les 5
+    const yTicks = generateTicks(minRange, maxRange, 5);
+    console.log(yTicks);
+
+    const chartConfig: ChartConfig = {
+        weight: {
+            label: "Poids",
+            color: "var(--color-chart-2)",
+        },
+    };
+
+    return (
+        <Card>
+            <CardContent>
+                <ChartContainer config={chartConfig} className="min-h-[300px]">
+                    <LineChart
+                        accessibilityLayer
+                        data={weightData}
+                        margin={{
+                            left: 12,
+                            right: 12,
+                        }}>
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="date"
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            tickFormatter={(value) => value.slice(5)}
+                        />
+                        <YAxis
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            domain={[minRange, maxRange]}
+                            ticks={yTicks}
+                        />
+                        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+                        <Line
+                            dataKey="weight"
+                            type="linear"
+                            stroke="var(--color-weight)"
+                            strokeWidth={1.5}
+                            dot={true}
+                        />
+                    </LineChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    );
+}
