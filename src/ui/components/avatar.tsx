9f0a89db258953097c6bfecc19ddea450c27afad
Clara Mauro <maurog.clara@gmail.com>
2025-07-11 18:43:18 +0200

    initialisation projet, début mise en page

---
+++ src/ui/components/avatar.tsx
@@ -0,0 +1,7 @@
+export default function Avatar() {
+    return (
+        <div className="bg-zinc-300 py-3 border shadow px-3 rounded-full text-white text-xl font-bold text-shadow-md">
+            CM
+        </div>
+    );
+}
---
+++ src/ui/layout/header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Avatar from "../components/avatar";
+
+export default function Header() {
+    return (
+        <header className="py-6 bg-white h-[105px] border-y border-gray-300 fixed top-0 left-0 right-0">
+            <div className="container mx-auto flex flex-wrap justify-between items-center gap-4">
+                <nav>
+                    <ul className="flex flex-wrap gap-4">
+                        <li>
+                            <Link href={"/"}>Tableau de bord</Link>
+                        </li>
+                        <li>
+                            <Link href={"/mesures"}>Mesures</Link>
+                        </li>
+                        {/* <li>
+                        <Link href={"/"}>Alimentation</Link>
+                    </li> */}
+                    </ul>
+                </nav>
+                <div className="flex items-center gap-4">
+                    <Avatar />
+                    <div>Déco</div>
+                </div>
+            </div>
+        </header>
+    );
+}
