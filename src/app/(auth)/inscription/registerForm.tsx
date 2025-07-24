2e9a168628b8e7807927371e6795a63724f076b0
Clara Mauro <maurog.clara@gmail.com>
2025-07-24 18:10:20 +0200

    validation formulaire inscription en cours

--- src/app/(auth)/inscription/registerForm.tsx
+++ src/app/(auth)/inscription/registerForm.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import * as z from "zod";
+import { RegisterField, registerSchema } from "@/libs/validation/authSchema";
 import Button from "@/ui/components/button";
 import { Input } from "@/ui/shadcn/components/ui/input";
 import { Label } from "@/ui/shadcn/components/ui/label";
 import { ChangeEvent, useState } from "react";
+import clsx from "clsx";
 
 export default function RegisterForm() {
     const [formData, setFormData] = useState({
@@ -14,10 +17,32 @@ export default function RegisterForm() {
         confirmPassword: "",
     });
 
+    const [formDataErrors, setFormDataErrors] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
     function handleChange(event: ChangeEvent<HTMLInputElement>) {
         const { id, value } = event.currentTarget;
-        console.log(id, value);
         setFormData((prev) => ({ ...prev, [id]: value }));
+        validateFields(id as RegisterField, value);
+    }
+
+    function validateFields(id: RegisterField, value: string) {
+        const validationSchema = registerSchema.shape[id];
+        const validationValue = validationSchema.safeParse(value);
+
+        if (!validationValue.success) {
+            const schemaError = z.flattenError(validationValue.error);
+            setFormDataErrors((prev) => ({ ...prev, [id]: schemaError.formErrors[0] }));
+            return;
+        }
+        setFormDataErrors((prev) => ({ ...prev, [id]: "" }));
+
+        // Gérer confirmPassword
     }
 
     return (
@@ -32,10 +57,14 @@ export default function RegisterForm() {
                         type="text"
                         name="firstname"
                         id="firstname"
-                        className={"focus-visible:ring-1 focus-visible:ring-main !text-lg"}
+                        className={clsx(
+                            "focus-visible:ring-1 focus-visible:ring-main !text-lg",
+                            formDataErrors?.firstname && "ring-2 ring-destructive focus-visible:ring-destructive"
+                        )}
                         onChange={handleChange}
                         value={formData.firstname}
                     />
+                    <div className="error-message mt-1">{formDataErrors?.firstname && formDataErrors?.firstname}</div>
                 </div>
                 <div className="mb-6 w-full">
                     <Label htmlFor="lastname" className="mb-2 text-md">
@@ -45,10 +74,14 @@ export default function RegisterForm() {
                         type="text"
                         name="lastname"
                         id="lastname"
-                        className={"focus-visible:ring-1 focus-visible:ring-main !text-lg"}
+                        className={clsx(
+                            "focus-visible:ring-1 focus-visible:ring-main !text-lg",
+                            formDataErrors?.lastname && "ring-2 ring-destructive focus-visible:ring-destructive"
+                        )}
                         onChange={handleChange}
                         value={formData.lastname}
                     />
+                    <div className="error-message mt-1">{formDataErrors?.lastname && formDataErrors?.lastname}</div>
                 </div>
                 <div className="mb-6 w-full">
                     <Label htmlFor="email" className="mb-2 text-md">
@@ -58,10 +91,14 @@ export default function RegisterForm() {
                         type="text"
                         name="email"
                         id="email"
-                        className={"focus-visible:ring-1 focus-visible:ring-main !text-lg"}
+                        className={clsx(
+                            "focus-visible:ring-1 focus-visible:ring-main !text-lg",
+                            formDataErrors?.email && "ring-2 ring-destructive focus-visible:ring-destructive"
+                        )}
                         onChange={handleChange}
                         value={formData.email}
                     />
+                    <div className="error-message mt-1">{formDataErrors?.email && formDataErrors?.email}</div>
                 </div>
                 <div className="mb-6 w-full">
                     <Label htmlFor="password" className="mb-2 text-md">
@@ -71,10 +108,18 @@ export default function RegisterForm() {
                         type="password"
                         name="password"
                         id="password"
-                        className={"focus-visible:ring-1 focus-visible:ring-main !text-lg"}
+                        className={clsx(
+                            "focus-visible:ring-1 focus-visible:ring-main !text-lg",
+                            formDataErrors?.password && "ring-2 ring-destructive focus-visible:ring-destructive"
+                        )}
                         onChange={handleChange}
                         value={formData.password}
                     />
+                    <div className="mt-1 text-gray-500 italic">
+                        Doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un
+                        caractère spécial.
+                    </div>
+                    <div className="error-message mt-1">{formDataErrors?.password && formDataErrors?.password}</div>
                 </div>
                 <div className="mb-6 w-full">
                     <Label htmlFor="confirmPassword" className="mb-2 text-md">
@@ -84,10 +129,17 @@ export default function RegisterForm() {
                         type="password"
                         name="confirmPassword"
                         id="confirmPassword"
-                        className={"focus-visible:ring-1 focus-visible:ring-main !text-lg"}
+                        className={clsx(
+                            "focus-visible:ring-1 focus-visible:ring-main !text-lg",
+                            formDataErrors?.confirmPassword && "ring-2 ring-destructive focus-visible:ring-destructive"
+                        )}
                         onChange={handleChange}
                         value={formData.confirmPassword}
                     />
+
+                    <div className="error-message mt-1">
+                        {formDataErrors?.confirmPassword && formDataErrors?.confirmPassword}
+                    </div>
                 </div>
                 <div className="text-center !text-lg">
                     <Button type={"submit"}>S&apos;inscrire</Button>
--- src/libs/validation/authSchema.ts
+++ src/libs/validation/authSchema.ts
@@ -4,3 +4,24 @@ export const loginSchema = z.object({
     email: z.email("Le format de l'email n'est pas valide"),
     password: z.string().min(1, "Le mot de passe est requis"),
 });
+
+type RegisterSchema = z.infer<typeof registerSchema>;
+export type RegisterField = keyof RegisterSchema;
+
+export const registerSchema = z
+    .object({
+        firstname: z.string().min(1, "Le prénom ne peut être vide"),
+        lastname: z.string().min(1, "Le nom ne peut être vide"),
+        email: z.email("Le format de l'email n'est pas valide"),
+        password: z
+            .string()
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\S]{8,}$/,
+                "Le mot de passe n'est pas valide"
+            ),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Les mots de passe ne correspondent pas",
+        path: ["confirmPassword"],
+    });
